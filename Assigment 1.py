e360c11ca8aae2a0472637aff74252e6b8d3deae
Chelo <lmmarulanda584@gmail.com>
2025-09-16 21:38:04 -0400

    added velocity function

--- Assigment 1.py
+++ Assigment 1.py
@@ -1,4 +1,3 @@
-
 # Air density
 P = 1e5
 R = 287
@@ -8,21 +7,27 @@ rho = P / (R * T)   # Air density in kg / m3
 
 import numpy as np
 import matplotlib.pyplot as plt
-import efficiencyCalculations
-import trajectoryCalculations
-#this is just filled with dummy values for now
-#Marcelo please update this to have points along the boundary
-boundary_matrix = np.array([
-    [0,0],
-    [0,1]
-])
-
 
 def cot(x):
     return 1 / np.tan(x)
 
+def air_velovity(x,y):
+    # Convert to polar coordinates
+    r = np.sqrt(x ** 2 + y ** 2)
+    theta = np.arctan2(y, x)
+
+    # Compute Vr and Vt
+    Vr = -np.sin(theta)
+    Vt = (1 / (2 * np.pi * r)) + np.cos(theta)
+
+    # Convert to Cartesian velocities
+    Vx = Vr * np.cos(theta) - Vt * np.sin(theta)
+    Vy = Vr * np.sin(theta) + Vt* np.cos(theta)
+
+    return (Vx, Vy)
+
 # Range of t (in radians)
-t = np.linspace(np.deg2rad(15.5568), np.deg2rad(176.891), 500)
+t = np.linspace(np.deg2rad(15.5568), np.deg2rad(176.891), 5000)
 
 # Curve 1
 C1 = 3.7699111843077517
@@ -42,98 +47,145 @@ y3 = -5*(np.pi - t) / (2*np.pi)
 x4 = (5*(np.pi - t) * cot(t)) / (2*np.pi)
 y4 = 5*(np.pi - t) / (2*np.pi)
 
-#Particle Trajectory -------------------------------------------------------------
-trajectoryCalculations.update_boundary_matrix(boundary_matrix)
-#calling with dummy values for now
-test_matrix = trajectoryCalculations.run_particle_simulation(startPos=[0,0],startVelocity=[2,4],startAirFlow=[1,2])
+# Target x-value
+target_x = -10
 
-# Define integration range only for Inlet
-mask_inlet = (x1 >= -10) & (x1 <= -0.8)
-# Define integration range for Scavenger (Outlet)
-mask_scavenger = (x1 > -0.8) & (x1 <= 10)
-# Define integration range for Outlet Core
-mask_core = (x2 > -0.8) & (x2 <= 10)
+# Find index where x is closest to target
+idx1 = np.argmin(np.abs(x1 - target_x))
+idx2 = np.argmin(np.abs(x2 - target_x))
 
-# Inlet area
-x_inlet = x1[mask_inlet]
-y_inlet = y1[mask_inlet] - y2[mask_inlet]
+# Get the corresponding points
+x1_near, y1_near = x1[idx1], y1[idx1]
+x2_near, y2_near = x2[idx2], y2[idx2]
 
-# Sort if necessary
-if x_inlet[0] > x_inlet[-1]:
-    x_inlet = x_inlet[::-1]
-    y_inlet = y_inlet[::-1]
-
-A_inlet = np.trapezoid(y_inlet, x_inlet)
-print(f"Inlet area = {A_inlet:.2f} m²")
-
-# Select x values and height for Scavenger
-x_scavenger = x1[mask_scavenger]
-y_scavenger = y1[mask_scavenger] - y4[mask_scavenger]
-
-# Sort if necessary
-if x_scavenger[0] > x_scavenger[-1]:
-    x_scavenger = x_scavenger[::-1]
-    y_scavenger = y_scavenger[::-1]
-
-# Integrate using trapezoid rule
-A_scavenger = np.trapezoid(y_scavenger, x_scavenger)
-print(f"Scavenger area = {A_scavenger:.2f} m²")
-
-# Select x values and height for Core
-x_core = x2[mask_core]
-y_core = y3[mask_core] - y2[mask_core]
-
-# Sort if necessary
-if x_core[0] > x_core[-1]:
-    x_core = x_core[::-1]
-    y_core = y_core[::-1]
-
-# Integrate using trapezoid rule
-A_core = np.trapezoid(y_core, x_core)
-print(f"Core area = {A_core:.2f} m²")
-
-
+# Air velocity
 V_inlet = 1.0  # m/s
-m_dot_inlet = rho * V_inlet * A_inlet
-print(f"Mass flow rate at inlet = {m_dot_inlet:.3f} kg/s")
 
-#post simmulation efficiency calculation
-#need to pass array of masses in each outlet here using dummy for now
-core = [5,10,1,5,8,10]
-scavenge = [6,4,7,10,1,5,7]
-ce = efficiencyCalculations.captureEfficiency(core, scavenge)
-print(f"Capture Efficency = {ce:.3f}")
+# Section length
+L_inlet = y1_near - y2_near
+
+# Mass flow rate
+m_dot_inlet = rho * V_inlet * L_inlet
+print(f"Mass flow rate at inlet = {m_dot_inlet:.4f} kg/s")
 
 
 
-# Plot
-plt.figure(figsize=(10,6))
-plt.plot(x1, y1, 'b', label='Curve I')
-plt.plot(x2, y2, 'g', label='Curve II')
-plt.plot(x3, y3, 'r', label='Curve III')
-plt.plot(x4, y4, 'orange', label='Curve IV')
-
-# Fill only the Inlet region
-plt.fill_between(x1[mask_inlet], y1[mask_inlet], y2[mask_inlet], color='skyblue', alpha=0.4, label='Inlet')
-# Fill the Scavenger region
-plt.fill_between(x1[mask_scavenger], y1[mask_scavenger], y4[mask_scavenger], color='lightgreen', alpha=0.4, label='Scavenger')
-# Fill the Outlet Core region
-plt.fill_between(x2[mask_core], y2[mask_core], y3[mask_core], color='salmon', alpha=0.4, label='Core')
-
-plt.axvline(0, color='k', linestyle='--', linewidth=1)
-plt.legend()
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Inertia particle separator")
-plt.grid(True)
-
-# Set axis limits
-plt.xlim(-10, 10)
-plt.ylim(-10, 10)
-
-plt.show()
+# Target x-value for curve 1
+target_x = 10
+idx = np.argmin(np.abs(x1 - target_x))
+x1_scavenger = x1[idx]
+y1_scavenger = y1[idx]
 
 
+# Convert to polar coordinates
+r1_scavenger = np.sqrt(x1_scavenger**2 + y1_scavenger**2)
+theta1_scavenger = np.arctan2(y1_scavenger, x1_scavenger)
+"""
+# Compute Vr and Vt
+Vr1_scavenger = -np.sin(theta1_scavenger)
+Vt1_scavenger = (1/(2*np.pi*r1_scavenger)) + np.cos(theta1_scavenger)
+
+# Convert to Cartesian velocities
+Vx1_scavenger = Vr1_scavenger*np.cos(theta1_scavenger) - Vt1_scavenger*np.sin(theta1_scavenger)
+Vy1_scavenger = Vr1_scavenger*np.sin(theta1_scavenger) + Vt1_scavenger*np.cos(theta1_scavenger)
+"""
+Vx1_scavenger, Vy1_scavenger = air_velovity(x1_scavenger,y1_scavenger)
+
+
+# Target x-value for curve 4
+target_x = 10
+idx4 = np.argmin(np.abs(x4 - target_x))
+x4_scavenger = x4[idx4]
+y4_scavenger = y4[idx4]
+
+# Convert to polar coordinates
+r4_scavenger = np.sqrt(x4_scavenger**2 + y4_scavenger**2)
+theta4_scavenger = np.arctan2(y4_scavenger, x4_scavenger)
+
+# Compute Vr and Vt
+Vr4_scavenger = -np.sin(theta4_scavenger)
+Vt4_scavenger = (1/(2*np.pi*r4_scavenger)) + np.cos(theta4_scavenger)
+
+# Convert to Cartesian velocities
+Vx4_scavenger = Vr4_scavenger*np.cos(theta4_scavenger) - Vt4_scavenger*np.sin(theta4_scavenger)
+Vy4_scavenger = Vr4_scavenger*np.sin(theta4_scavenger) + Vt4_scavenger*np.cos(theta4_scavenger)
+
+# Average Y at outlet (Scavenger)
+y_avg_scavenger = (y1_scavenger + y4_scavenger) / 2
+
+
+# Average Vx at outlet (Scavenger)
+Vx_avg_scavenger = (Vx1_scavenger + Vx4_scavenger) / 2
+Vy_avg_scavenger = (Vy1_scavenger + Vy4_scavenger) / 2
+
+# Outlet section length
+L_outlet = y1_scavenger - y4_scavenger
+
+# Mass flow rate at Scavenger outlet
+m_dot_scavenger = rho * Vx_avg_scavenger * L_outlet
+
+print(f"\nVelocity at Outlet (Scavenger) At x = {10} m, and y = {y_avg_scavenger:.3f} m: Vx = {Vx_avg_scavenger:.3f} m/s, Vy = {Vy_avg_scavenger:.3f} m/s")
+
+print(f"Mass flow rate at Scavenger outlet = {m_dot_scavenger:.4f} kg/s")
+
+
+
+# Target x-value for curve 2
+target_x = 10
+idx2 = np.argmin(np.abs(x2 - target_x))
+x2_core = x2[idx2]
+y2_core = y2[idx2]
+
+# Convert to polar coordinates
+r2_core = np.sqrt(x2_core**2 + y2_core**2)
+theta2_core = np.arctan2(y2_core, x2_core)
+
+# Compute Vr and Vt
+Vr2_core = -np.sin(theta2_core)
+Vt2_core = (1/(2*np.pi*r2_core)) + np.cos(theta2_core)
+
+# Convert to Cartesian velocities
+Vx2_core = Vr2_core*np.cos(theta2_core) - Vt2_core*np.sin(theta2_core)
+Vy2_core = Vr2_core*np.sin(theta2_core) + Vt2_core*np.cos(theta2_core)
+
+
+# Target x-value for curve 3
+target_x = 10
+idx3 = np.argmin(np.abs(x3 - target_x))
+x3_core = x3[idx3]
+y3_core = y3[idx3]
+
+# Convert to polar coordinates
+r3_core = np.sqrt(x3_core**2 + y3_core**2)
+theta3_core = np.arctan2(y3_core, x3_core)
+
+# Compute Vr and Vt
+Vr3_core = -np.sin(theta3_core)
+Vt3_core = (1/(2*np.pi*r3_core)) + np.cos(theta3_core)
+
+# Convert to Cartesian velocities
+Vx3_core = Vr3_core*np.cos(theta3_core) - Vt3_core*np.sin(theta3_core)
+Vy3_core = Vr3_core*np.sin(theta3_core) + Vt3_core*np.cos(theta3_core)
+
+
+# Average Y at outlet (Core)
+y_avg_core = (y2_core + y3_core) / 2
+
+# Average Vx at outlet (Core)
+Vx_avg_core = (Vx2_core + Vx3_core) / 2
+Vy_avg_core = (Vy2_core + Vy3_core) / 2
+
+# Outlet section length (Core)
+L_outlet_core = y3_core - y2_core
+
+
+# Mass flow rate at Core outlet
+m_dot_core = rho * Vx_avg_core * L_outlet_core
+
+print(f"\nVelocity at Outlet (Core) At x = {10} m, and y = {y_avg_core:.3f} m: Vx = {Vx_avg_core:.3f} m/s, Vy = {Vy_avg_core:.3f} m/s")
+
+print(f"Mass flow rate at Core outlet = {m_dot_core:.4f} kg/s")
+############################################################################################################################################
 
 
 
